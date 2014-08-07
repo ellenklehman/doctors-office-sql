9d4a6ca7bd3fb147989d56a0ea2c5e0b2e065380
Ellen Krause Lehman <student@epicodus.com>
2014-08-07 10:38:16 -0700

    tell you a patients name, birthdate, doctor id, and id

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,12 @@
 class Patient
 
-  attr_reader :name, :birthdate, :doctor_id
+  attr_reader :name, :birthdate, :doctor_id, :id
 
   def initialize(attributes)
     @name = attributes['name']
     @birthdate = attributes['birthdate']
     @doctor_id = attributes['doctor_id']
+    @id = attributes['id']
   end
 
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 describe 'Patient' do
   describe 'initialize' do
     it 'initializes a patient with a name, doctor id, and a birthdate' do
-      test_patient = Patient.new({'name' => 'Vanilla Ice', 'birthdate' => '1975-01-15', 'doctor_id' => 1})
+      test_patient = Patient.new({'name' => 'Vanilla Ice', 'birthdate' => '1975-01-15', 'doctor_id' => 1, 'id' => 1})
       expect(test_patient).to be_an_instance_of Patient
     end
+  end
+
+  it 'tells you its name, birthdate, doctor id and id' do
+    test_patient = Patient.new({'name' => 'Vanilla Ice', 'birthdate' => '1975-01-15', 'doctor_id' => 1, 'id' => 1})
+    expect(test_patient.name).to eq 'Vanilla Ice'
+    expect(test_patient.birthdate).to eq '1975-01-15'
+    expect(test_patient.doctor_id).to eq 1
+    expect(test_patient.id).to eq 1
   end
 end
